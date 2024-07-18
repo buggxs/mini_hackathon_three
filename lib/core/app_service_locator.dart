f006251fdbfd29a46704d061aec4139abb5ce266
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2024-07-18 19:48:36 +0200

    feat(load_movie): loading movie from api

---
+++ lib/core/app_service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:get_it/get_it.dart';
+import 'package:mini_hackathon_three/api/movie/movie_service.dart';
+
+final GetIt app = GetIt.instance;
+
+void setup() {
+  // For testing purposes
+  app.allowReassignment = true;
+
+  app.registerFactory<MovieService>(OnlineMovieService.new);
+}
